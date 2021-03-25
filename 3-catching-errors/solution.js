26c3b7d1a7847422cb2fc0f827ca98925ed06a37
Yvonne Lange <yvonne-lange@web.de>
2021-03-25 14:42:03 +0100

    Done #4

--- 3-catching-errors/solution.js
+++ 3-catching-errors/solution.js
@@ -3,12 +3,6 @@ const url = "https://.typicode.com/todos/x";
 fetch(url)
   .then((response) => {
     return response.json();
-    // if (response.ok) {
-    // } else {
-    //   console.log(response.status);
-
-    //   throw new Error("Error status: ", response.status);
-    // }
   })
   .then((data) => {
     console.log(data);
